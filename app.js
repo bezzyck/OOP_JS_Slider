d49d003308ef366919e8989a98ef562a41ef337a
Alex Tarasov <bezzyck@yandex.ru>
2020-08-26 14:35:41 +0300

    refactor

--- app.js
+++ app.js
@@ -4,20 +4,18 @@ let slider = {
 	btnNext: document.querySelector('.next'),
 	imgs: ['img/img_1.jpg', 'img/img_2.jpg', 'img/img_3.jpg'],
 	zero: 0,
-	firstImg: function () {
-		this.slideImg.src = this.imgs[this.zero]
-	},
-	clickOnBack: function () {
-		this.btnBack.addEventListener('click', () => {
+	start: function () {
+		firstImg: this.slideImg.src = this.imgs[this.zero]
+
+		clickOnBack: this.btnBack.addEventListener('click', () => {
 			if (this.zero == 0) {
 				this.zero += this.imgs.length;
 			}
 			this.zero--;
 			this.slideImg.src = this.imgs[this.zero];
 		})
-	},
-	clickOnNext: function () {
-		this.btnNext.addEventListener('click', () => {
+
+		clickOnNext: this.btnNext.addEventListener('click', () => {
 			if (this.zero == this.imgs.length - 1) {
 				this.zero -= this.imgs.length;
 			}
@@ -25,10 +23,6 @@ let slider = {
 			this.slideImg.src = this.imgs[this.zero];
 		})
 	}
-
-
 }
 
-slider.firstImg();
-slider.clickOnNext();
-slider.clickOnBack();
+slider.start()
